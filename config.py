30868bf859acc7065d429535cb73cc0066fbfab5
PLAYZHACKING <playzhacking12946@gmail.com>
2025-06-11 13:18:15 +0530

    config.py

--- config.py
+++ config.py
@@ -28,10 +28,10 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "PLAYZ_FILE_STORE_BOT") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://play-z-hacking:No-name-no@cluster0.dogzhpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
